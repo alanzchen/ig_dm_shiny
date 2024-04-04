934adf5fba5151fb919ab485e94821a381fe1dff
Alan Chen <github@zenan.ch>
2024-04-04 12:23:10 -0500

    Update thread

--- wx_app.py
+++ wx_app.py
@@ -4,7 +4,6 @@ import wx
 import wx.lib.newevent
 import pandas as pd
 import threading
-import concurrent.futures
 from ig_dm_scraper.scraper import get_dm_from_zip
 from ig_dm_scraper.formatter import _get_message_text, _get_message_type, _get_reaction
 from sentence_transformers import SentenceTransformer
@@ -73,35 +72,7 @@ class MainFrame(wx.Frame):
             self.open_file_button.Disable()
             self.SetStatusText("Processing... Please wait.")
             # Async processing
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = executor.submit(self.load_data, file_dialog.GetPath())
-                future.add_done_callback(self.on_data_loaded)
-    
-    def load_data(self, path):
-        self.open_file_button.Disable()
-        try:
-            # get the date one month before now
-            one_month_ago = datetime.now() - timedelta(days=30)
-            raw = get_dm_from_zip(filepath=path, oldest_date=one_month_ago.strftime('%Y-%m-%d'))
-            print("reformat")
-            # Use ReformatThread to run reformat in a separate thread
-            ReformatThread(self, raw, as_dataframe=True)
-        except Exception as e:
-            wx.MessageBox(f"{e}", "Error", wx.OK | wx.ICON_ERROR)
-            wx.CallAfter(self.SetStatusText, f"Error")
-            self.open_file_button.Enable()
-            return None
-
-    def on_data_loaded(self, future):
-        try:
-            self.raw_df = future.result()
-            if self.raw_df is not None:
-                self.SetStatusText("Processing...")
-                self.save_file_button.Enable()
-            else:
-                self.SetStatusText("")
-        except Exception as e:
-            self.SetStatusText(f"Error processing file: {e}")
+            ReformatThread(self, file_dialog.GetPath(), as_dataframe=True)
 
     def on_save_file(self, event):
         with wx.FileDialog(self, "Save file", defaultFile="please_upload_me_to_webpage", wildcard="Parquet files (*.parquet)|*.parquet",
@@ -119,62 +90,72 @@ class MainFrame(wx.Frame):
                 self.SetStatusText(f"Error saving the file: {e}")
 
     def on_reformat_done(self, result):
-        # Assuming 'result' is a DataFrame or None
-        if result is not None and not result.empty:
-            self.raw_df = result  # Store the DataFrame in the instance
-            wx.CallAfter(self.save_file_button.Enable)  # Enable the save button on the main thread
-            wx.CallAfter(self.SetStatusText, "Step 2 compeleted.")
-        else:
-            wx.MessageBox("We could not process your file. The file is in correct format but an unknown error has occurred. A possible reason is that the export data does not contain any message data in the past month. Please contact the study administrator chen6029@umn.edu for next steps.", "Error", wx.OK | wx.ICON_ERROR)
-            wx.CallAfter(self.SetStatusText, "Failed to process the file or no data returned.")
+        self.raw_df = result  # Store the DataFrame in the instance
     
     def update_progress_bar(self, progress):
         self.progress_bar.SetValue(progress)
 
 
 class ReformatThread(threading.Thread):
-    def __init__(self, parent, threads, as_dataframe=False):
+    def __init__(self, parent, path, as_dataframe=False):
         threading.Thread.__init__(self)
         self.parent = parent
-        self.threads = threads
         self.as_dataframe = as_dataframe
+        self.path = path
         self.start()  # start the thread
 
     def run(self):
         # Execute the reformat function in the thread
-        output = self.reformat(self.threads, self.as_dataframe)
+        output = self.reformat(self.path, self.as_dataframe)
         wx.CallAfter(self.parent.on_reformat_done, output)
 
-    def reformat(self, threads: list[dict], as_dataframe=False):
-        model = SentenceTransformer("all-MiniLM-L6-v2")
-        output = []
-        total_messages = sum(len(thread['message']) for thread in threads)
-        messages_processed = 0
-        for thread_idx, thread in enumerate(threads):
-            for message in thread['message']:
-                text = _get_message_text(message)
-                type = _get_message_type(message)
-                output.append(
-                    {
-                    'thread_id': thread_idx,
-                    'sender_name': hashlib.md5(message['sender_name'].encode('raw_unicode_escape')).hexdigest(),
-                    'timestamp': datetime.fromtimestamp(message['timestamp_ms']/ 1000).strftime('%Y-%m-%d %H:%M:%S'),
-                    'message_type': type,
-                    # 'text': text if type == 'text' else None,
-                    'len': len(text) if type == 'text' else None,
-                    'words': len(text.split()) if type == 'text' else None,
-                    'embedding': model.encode(text) if type == 'text' else None,
-                    'reaction': _get_reaction(message)
-                    }
-                )
-                messages_processed += 1
-                progress = int((messages_processed / total_messages) * 100)
-                wx.CallAfter(self.parent.update_progress_bar, progress)
-        
-        if as_dataframe:
-            output = pd.DataFrame(output)
+    def reformat(self, path, as_dataframe=False):
+        try:
+            model = SentenceTransformer("all-MiniLM-L6-v2")
+            # get the date one month before now
+            one_month_ago = datetime.now() - timedelta(days=30)
+            threads = get_dm_from_zip(filepath=path, oldest_date=one_month_ago.strftime('%Y-%m-%d'))
+            output = []
+            total_messages = sum(len(thread['message']) for thread in threads)
+            messages_processed = 0
+            for thread_idx, thread in enumerate(threads):
+                for message in thread['message']:
+                    text = _get_message_text(message)
+                    type = _get_message_type(message)
+                    output.append(
+                        {
+                        'thread_id': thread_idx,
+                        'sender': hashlib.md5(message['sender_name'].encode('raw_unicode_escape')).hexdigest()[:5] if message['sender_name'] != 'participant' else 'participant',
+                        'timestamp': datetime.fromtimestamp(message['timestamp_ms']/ 1000).strftime('%Y-%m-%d %H:%M:%S'),
+                        'message_type': type,
+                        # 'text': text if type == 'text' else None,
+                        'len': len(text) if type == 'text' else None,
+                        'words': len(text.split()) if type == 'text' else None,
+                        'embedding': model.encode(text) if type == 'text' else None,
+                        'reaction': _get_reaction(message)
+                        }
+                    )
+                    messages_processed += 1
+                    progress = int((messages_processed / total_messages) * 100)
+                    wx.CallAfter(self.parent.update_progress_bar, progress)
+            
+            if as_dataframe:
+                output = pd.DataFrame(output)
 
-        return output
+            if output is not None and not output.empty:
+                wx.CallAfter(self.parent.save_file_button.Enable)  # Enable the save button on the main thread
+                wx.CallAfter(self.parent.SetStatusText, "Step 2 compeleted.")
+            else:
+                wx.MessageBox("We could not process your file. The file is in correct format but an unknown error has occurred. A possible reason is that the export data does not contain any message data in the past month. Please contact the study administrator chen6029@umn.edu for next steps.", "Error", wx.OK | wx.ICON_ERROR)
+                wx.CallAfter(self.parent.SetStatusText, "Failed to process the file or no data returned.")
+                self.parent.open_file_button.Enable()
+            return output
+        
+        except Exception as e:
+            wx.CallAfter(self.parent.SetStatusText, f"Error processing the file: {e}")
+            wx.MessageBox(f"Error processing the file: {e}", "Error", wx.OK | wx.ICON_ERROR)
+            self.parent.open_file_button.Enable()
+            return None
 
 def main():
     app = wx.App(False)
